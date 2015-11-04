1b708499c0809d3980718f25654fb1ace79cd530
Muhammad Yasmin <yasminmuh2615@gmail.com>
2015-11-04 19:36:19 +0700

    Fix
    Tambah Halaman Admin, Daftar

---
+++ pages/daftar/index.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Website Jurusan RPL</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="shortcut icon" href="../../images/logo.png" />
+	<link rel="stylesheet" href="../../assets/css/main.css">
+	<link rel="stylesheet" href="../../assets/css/font-awesome.min.css">
+
+</head>
+<body>
+	<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="../../">Website RPL</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+										<li><a href="../../">Home</a></li>
+											<li><a href="../profil">Profil</a></li>
+											<li><a href="../guru">Data Guru</a></li>
+											<li><a href="#">Data Siswa</a></li>
+											<li><a href="../about">Tentang Kami</a></li>
+											<li><a href="#">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Daftar</h2>
+						</header>
+						<section class="wrapper style5">
+							<div class="inner" style="margin-bottom:470px;">
+								<section class="gae-daftar">
+									<h4>Daftar</h4>
+									<p>Dengan membuat akun baru, Anda akan dapat mendapatkan informasi terbaru dari jurusan, menyimpan banyak data berharga, melihat dan mencari data dalam akun Anda dan banyak lagi.</p>
+								</section>
+								<div>
+									<form action="" method="post">
+										<table border="40" class="tbl-daftar">
+											<tr>
+												<td><label for="">NISN</label></td>
+												<td><input type="text" name="nisn" class="form-control" placeholder="Inputkan NISN" autocomplete="off"></td>
+											</tr>
+											<tr>
+												<td><label for="">Nama Lengkap</label></td>
+												<td colspan="3"><input type="text" name="nm-lkp" class="form-control" placeholder="Inputkan Nama Lengkap" autocomplete="off"></td>
+											</tr>
+											<tr>
+												<td><label for="">Alamat</label></td>
+												<td colspan="3"><input type="text" name="alamat" class="form-control" placeholder="Inputkan Alamat" autocomplete="off"></td>
+											</tr>
+											<tr>
+												<td><label for="">Email</label></td>
+												<td colspan="3"><input type="email" name="email" class="form-control" placeholder="Inputkan Alamat" autocomplete="off"></td>
+												
+											</tr>
+											<tr>
+												<td><label for="">No. Handphone</label></td>
+												<td colspan="3"><input type="text" name="nohape" class="form-control" placeholder="Inputkan Alamat" autocomplete="off"></td>
+												
+											</tr>
+											<tr>
+												<td colspan="3"><label for="">Orang Tua</label></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td><label for="">Nama Ibu</label></td>
+												<td><input type="text" name="nama-ibu" class="form-control" placeholder="Inputkan Alamat" autocomplete="off"></td>
+											</tr>
+										</table>
+									</form>
+								</div>
+							</div>
+						</section>
+					</article>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright All Reserved &copy; 2015 RPL</li><li><a href="http://www.smkn5malang.sch.id">SMK Negeri 5 Malang</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="../../assets/js/jquery.min.js"></script>
+			<script src="../../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../../assets/js/skel.min.js"></script>
+			<script src="../../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../../assets/js/main.js"></script>
+</body>
+</html>
